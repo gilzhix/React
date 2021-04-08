4308c1f97128b7bc07f9c238874cf4282f76ff41
Gilbert Cachaya <gcachay49166@universidadean.edu.co>
2021-04-07 21:03:15 -0500

    reestructuracion y creacion de react app

---
+++ nivelacion java/destructuring.js
@@ -0,0 +1,10 @@
+const Persona = {
+    nombre: 'Gilbert',
+    edad: 26,
+    casado: false,
+    hijo:{
+        nombrehijo:'Tequila'
+    }
+}
+const {nombrehijo}= Persona.hijo
+console.log(nombrehijo)
---
+++ nivelacion java/map.js
@@ -0,0 +1,10 @@
+const datos = [
+    {nombre: 'Gilbert', edad:26},
+    {nombre: 'victoria', edad:32},
+    {nombre: 'fabian', edad:48},
+    {nombre: 'Ana', edad:78},
+]
+
+datos.map ( elemento => {
+    console.log(elemento.nombre)
+}) 
