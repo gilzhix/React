3fa3a5e420c2bbdb6c373530a55ee384e88849cd
Gilbert Cachaya <gcachay49166@universidadean.edu.co>
2021-04-07 21:21:27 -0500

    reestructuracion de fomatos y componente1

--- REACT/primerproyecto/src/App.jsx
+++ REACT/primerproyecto/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './App.css';
+import Hola from './components/Hola'
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>Titulo de nuestro primer proyecto</h1>
+      <Hola></Hola>
     </div>
   );
 }
